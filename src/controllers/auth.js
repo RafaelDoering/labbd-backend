283b472fbebd2f2bafa424c3ebd02d0ea05ece54
RafaelDoering <rafael.doering.soares@usp.br>
2020-06-24 18:28:53 -0300

    hash password

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -3,38 +3,33 @@ const jwt = require('jsonwebtoken');
 const { pool } = require('../utils/db');
 
 module.exports.login = async (req, res) => {
-  const {
+  let {
     userId,
+  } = req.body;
+  const {
     password,
   } = req.body;
 
-  let user;
   if (userId === 'adminCovid') {
-    if (password !== 'superAdmin') {
-      return res.status(401).json();
-    }
-    user = {
-      id: 9999999,
-      departamento: 'Admin',
-    };
-  } else {
-    const query = `
-      SELECT funcionario.id_funcionario as id, funcionario.departamento as departamento, credencial.senha as senha
-      FROM public.funcionario as funcionario
-      LEFT JOIN public.credencial as credencial
-      ON funcionario.login=credencial.login
-      WHERE funcionario.id_funcionario=${userId};
-    `;
-    const queryResponse = await pool.query(query);
-    // eslint-disable-next-line prefer-destructuring
-    user = queryResponse.rows[0];
-    if (!user || password !== user.senha) {
-      return res.status(401).json();
-    }
+    userId = 9999999;
   }
 
-  const query = 'INSERT INTO logs(id_func, reg_hora) VALUES ($1, $2)';
-  await pool.query(query, [user.id, new Date()]);
+  const query = `
+    SELECT funcionario.id_funcionario as id, funcionario.departamento as departamento, credencial.senha as senha, password_hash('${password}') as password_attempt
+    FROM public.funcionario as funcionario
+    LEFT JOIN public.credencial as credencial
+    ON funcionario.login=credencial.login
+    WHERE funcionario.id_funcionario=${userId};
+  `;
+  const queryResponse = await pool.query(query);
+  // eslint-disable-next-line prefer-destructuring
+  const user = queryResponse.rows[0];
+  if (!user || user.password_attempt !== user.senha) {
+    return res.status(401).json();
+  }
+
+  const queryLog = 'INSERT INTO logs(id_func, reg_hora) VALUES ($1, $2)';
+  await pool.query(queryLog, [user.id, new Date()]);
 
   const token = jwt.sign({
     data: { id: user.id, departamento: user.departamento },
